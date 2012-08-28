f0533d91a7ba13948d6043c048da519e0f62573e
Robin Smidsrød <robin@smidsrod.no>
2012-08-28 18:04:01 +0200

    Added a language choice example binary

---
+++ bin/language_choice.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use rlib;
+use Unicode::CLDR;
+use feature 'say';
+
+STDOUT->binmode("utf8");
+
+my $cldr = Unicode::CLDR->new();
+my $en = $cldr->get_locale('en');
+my @languages = map { $_->id } $en->all_languages;
+foreach my $language_id (sort @languages ) {
+    my $locale = eval { $cldr->get_locale( $language_id ); };
+    next unless $locale;
+    my $lang = $locale->get_language( $language_id );
+    next unless $lang;
+    say "id=" . $lang->id . ", name=" . $lang->name;
+}
